44ddfa74fa9fff53d8c3784018867cfd7eba8a66
SiddikMulla <sidmm747@gmail.com>
2025-08-31 23:30:05 +0530

    added job listing cards with Json data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
   <Navbar />
-  <Hero title="Test" subTitle="test" />
+  <Hero title="Become a Vue Dev" subtitle="Find the Vue job that fits your skills and needs" />
+  <HomeCards />
+  <JobListings limit="3" :show-button="true" />
 </template>
