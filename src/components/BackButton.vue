1ab0c5a94276f218086d74e6cf2b128f00e34637
SiddikMulla <sidmm747@gmail.com>
2025-09-02 00:17:04 +0530

    feat: added back button

---
+++ src/components/BackButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+    <section>
+        <div class="container m-auto py-6 px-6">
+            <RouterLink to="/jobs" class="text-green-600 hover:text-green-600 text-xl flex items-center">
+                <i class="pi pi-arrow-circle-left mr-2"></i> Back to Job Listings
+            </RouterLink>
+        </div>
+    </section>
+
+</template>
