fca23d52ddcc2d64af04f7065d583d642297c958
SiddikMulla <sidmm747@gmail.com>
2025-09-02 00:17:43 +0530

    refactor :ui changes

--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -17,7 +17,7 @@ import { RouterLink } from 'vue-router';
                         Browse Jobs
                     </RouterLink>
                 </Card>
-                <Card bg="bg-green-100">
+                <Card bg="bg-green-50">
                     <h2 class="text-2xl font-bold">For Employers</h2>
                     <p class="mt-2 mb-4">
                         List your job to find the perfect developer for the role
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,7 +9,7 @@ const isActiveLink = (routerPath) => {
 </script>
 
 <template>
-    <nav class="bg-green-800 border-b border-green-700">
+    <nav class="bg-green-700 border-b border-green-600">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="flex h-20 items-center justify-between">
                 <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
